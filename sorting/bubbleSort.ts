1985d2fb06b79823cc70360e65134f409b56a2e5
MicroFish91 <mwfisher91@gmail.com>
2021-09-02 20:42:28 -0500

    Bubble sort

---
+++ sorting/bubbleSort.ts
@@ -0,0 +1,28 @@
+/*
+ * In bubble sort, the largest value will bubble to the top.  Because
+ * of this, we do not need to run through to the end of the array each time.
+ * After each pass we can reduce the length of the for loop by 1.
+ */
+
+function bubbleSort(numList: number[]): number[] {
+  let swapped;
+  let length = numList.length - 1;
+
+  while (true) {
+    swapped = false;
+    for (let i = 0; i < length; i++) {
+      if (numList[i] > numList[i + 1]) {
+        [numList[i], numList[i + 1]] = [numList[i + 1], numList[i]];
+        swapped = true;
+      }
+    }
+
+    length--;
+
+    if (!swapped) {
+      return numList;
+    }
+  }
+}
+
+console.log(bubbleSort([6, 1, 89, 24, 43, 3, 1, 19]));
