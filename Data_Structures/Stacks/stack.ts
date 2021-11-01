4c7129e60b09935636f998354134683f29e124b6
Matthew Fisher <mwfisher91@gmail.com>
2021-11-01 10:04:43 -0700

    stack implementation

--- Data_Structures/Stacks/stack.ts
+++ Data_Structures/Stacks/stack.ts
@@ -1,17 +1,7 @@
-interface Stack {
-  length: number;
-  start: NodeProtoType;
-  end: NodeProtoType;
-}
-
-interface NodeProtoType {
-  val: any;
-  next: NodeProtoType;
-}
 //@ts-ignore
-class NodeProto {
+class StackElement {
   public val: any;
-  public next: NodeProtoType;
+  public next: StackElement;
 
   constructor(val: any) {
     this.val = val;
@@ -23,69 +13,53 @@ class NodeProto {
 // Here we use push and pop as basically sub-ins for shift and unshift
 // This is because shift and unshift are O(1) for SLL and O(n) for classic push/pop implementation
 class Stack {
+  public bottom: StackElement;
+  public top: StackElement;
   public length: number;
-  public start: NodeProtoType;
-  public end: NodeProtoType;
 
   constructor() {
+    this.top = null;
+    this.bottom = null;
     this.length = 0;
-    this.start = null;
-    this.end = null;
   }
 
-  push(val: any) {
-    const newNode = new NodeProto(val);
+  push(val: any): number {
+    const newElement = new StackElement(val);
 
-    if (this.length === 0) {
-      this.start = newNode;
-      this.end = newNode;
+    if (this.top === null) {
+      this.top = newElement;
+      this.bottom = newElement;
       return ++this.length;
     }
 
-    const start = this.start;
-    newNode.next = start;
-    this.start = newNode;
+    const originalTop = this.top;
+    this.top = newElement;
+    this.top.next = originalTop;
 
     return ++this.length;
   }
 
-  pop() {
-    if (this.length === 0) {
-      return null;
+  pop(): any {
+    if (this.top === null) {
+      return undefined;
     }
 
-    const start = this.start;
+    const returnNode = this.top;
 
     if (this.length === 1) {
-      this.start = null;
-      this.end = null;
+      this.top = null;
+      this.bottom = null;
       this.length--;
-      return start.val;
+      return returnNode.val;
     }
 
-    this.start = this.start.next;
+    const nextNode = this.top.next;
+    this.top = nextNode;
     this.length--;
-    return start.val;
+    return returnNode.val;
   }
 
-  peek() {
-    if (this.length === 0) {
-      return null;
-    }
-
-    return this.start.val;
+  peek(): any {
+    return this.top.val;
   }
 }
-
-const stack = new Stack();
-
-stack.push(1);
-stack.push(2);
-stack.push(3);
-
-console.log("Peeking: ", stack.peek());
-
-console.log(stack.pop());
-console.log(stack.pop());
-console.log(stack.pop());
-console.log(stack.pop());
