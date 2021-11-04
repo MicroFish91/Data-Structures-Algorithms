d15c609d19dde75d8dd35e2e8c6771fc7bbc71c5
Matthew Fisher <mwfisher91@gmail.com>
2021-11-04 15:12:16 -0700

    Heap methods

---
+++ Data_Structures/Trees/Heaps/BinaryHeaps/MaxBinaryHeap.ts
@@ -0,0 +1,117 @@
+class MaxBinaryHeap {
+  public values: number[];
+
+  constructor() {
+    this.values = [];
+  }
+
+  extractMax() {
+    if (!this.values.length) return null;
+
+    [this.values[0], this.values[this.values.length - 1]] = [
+      this.values[this.values.length - 1],
+      this.values[0],
+    ];
+
+    const extractedVal = this.values.pop();
+
+    this.bubbleDown(0);
+    return extractedVal;
+  }
+
+  // [30, 29, 28, 27, 26, 25] 31
+  //  0   1   2   3   4   5   6
+  insert(val: number): number[] {
+    this.values.push(val);
+    this.bubbleUp();
+    return this.values;
+  }
+
+  bubbleDown(idx: number) {
+    let childOne, childTwo, childOneIdx, childTwoIdx, currentVal, swapIdx;
+
+    while (true) {
+      currentVal = this.values[idx];
+      childOneIdx = 2 * idx + 1;
+      childTwoIdx = 2 * idx + 2;
+      swapIdx = undefined;
+
+      childOne =
+        childOneIdx < this.values.length ? this.values[childOneIdx] : undefined;
+      childTwo =
+        childTwoIdx < this.values.length ? this.values[childTwoIdx] : undefined;
+
+      // If no children, just swap to end
+      if (!childOne && !childTwo) {
+        [this.values[idx], this.values[this.values.length - 1]] = [
+          this.values[this.values.length - 1],
+          this.values[idx],
+        ];
+        return;
+      }
+
+      // If childOne exists, set to swapIdx
+      if (childOne) {
+        if (childOne > currentVal) swapIdx = childOneIdx;
+      }
+
+      // If childTwo exists and is greater, set to swapIdx
+      if (childTwo) {
+        if (childTwo > childOne && childTwo > currentVal) {
+          swapIdx = childTwoIdx;
+        }
+      }
+
+      if (!swapIdx) return;
+
+      // Swap values at current idx and swapIdx
+      [this.values[idx], this.values[swapIdx]] = [
+        this.values[swapIdx],
+        this.values[idx],
+      ];
+
+      // Set idx to old swapIdx
+      idx = swapIdx;
+    }
+  }
+
+  bubbleUp() {
+    if (this.values.length === 1) return;
+
+    let currentIndex = this.values.length - 1;
+    let parentIndex;
+
+    while (true) {
+      parentIndex = Math.floor((currentIndex - 1) / 2);
+      if (this.values[currentIndex] > this.values[parentIndex]) {
+        [this.values[currentIndex], this.values[parentIndex]] = [
+          this.values[parentIndex],
+          this.values[currentIndex],
+        ];
+      }
+      if (!parentIndex) return;
+
+      currentIndex = parentIndex;
+    }
+  }
+}
+
+const mbh = new MaxBinaryHeap();
+
+mbh.insert(41);
+mbh.insert(39);
+mbh.insert(33);
+mbh.insert(18);
+mbh.insert(27);
+mbh.insert(12);
+mbh.insert(55);
+
+console.log(mbh);
+console.log(mbh.extractMax());
+console.log(mbh);
+console.log(mbh.extractMax());
+console.log(mbh);
+console.log(mbh.extractMax());
+console.log(mbh);
+console.log(mbh.extractMax());
+console.log(mbh);
---
+++ Data_Structures/Trees/Heaps/BinaryHeaps/README.md
@@ -0,0 +1,38 @@
+# Binary Heaps
+
+![Binary Heap](https://media.geeksforgeeks.org/wp-content/cdn-uploads/MinHeapAndMaxHeap.png)
+
+A Heap is a special Tree-based data structure in which the tree is a complete binary tree; that is, all levels of the tree, except possibly the last one (deepest) are fully filled, and, if the last level of the tree is not complete, the nodes of that level are filled from left to right.
+
+Generally, Heaps can be of two types:
+
+- Max-Heap: In a Max-Heap the key present at the root node must be greatest among the keys present at all of it’s children. The same property must be recursively true for all sub-trees in that Binary Tree.
+
+- Min-Heap: In a Min-Heap the key present at the root node must be minimum among the keys present at all of it’s children. The same property must be recursively true for all sub-trees in that Binary Tree.
+
+Child -> Parent => Math.floor( (n-1) / 2 )
+Parent -> Child => (2n+1) & (2n+2)
+
+## Insertion
+
+![Insertion](https://www.algolist.net/img/binary-heap-insertion-2.png)
+
+Add a new element to the end of an array
+Bubble up the new element. Bubbling is done as following: compare node's value with parent's value. If they are in wrong order, swap them.
+
+## Deletion (extractMax)
+
+The standard deletion operation on Heap is to delete the element present at the root node of the Heap. That is if it is a Max Heap, the standard deletion operation will delete the maximum element and if it is a Min heap, it will delete the minimum element.
+
+Process:
+
+- Replace the root or element to be deleted by the last element.
+- Pop from the values property, so you can return value at end
+- Have the new root or element sink down to the correct spot
+  - Your parent index starts at 0 (the root)
+  - Find the index of the left child: 2 \* index + 1
+  - Find the index of the right child: 2 \* index + 1
+  - If the left or right child is greater than the element... swap. If both are larger, swap with the largest child
+  - The child index you swapped to now becomes the new parent index
+  - Keep looping and swapping until neither child is larger than the element
+  - Return the old root
